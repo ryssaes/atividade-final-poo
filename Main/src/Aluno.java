0951fabad5d6c0ea9176153cefffb06137be4d1d
Ryssa Sobreira <ryssateles@gmail.com>
2025-05-05 22:30:01 -0300

    create basic Aluno class for Turma association

---
+++ Main/src/Aluno.java
@@ -0,0 +1,14 @@
+package Main.src;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() { return nome; }
+    public String getMatricula() { return matricula; }
+}
