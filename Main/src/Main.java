283d4c83a8471780e4ed7662bca65503a4b9f9c4
Ryssa Sobreira <ryssateles@gmail.com>
2025-05-05 22:33:03 -0300

    create Main class to test Turma functionality

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,17 @@
+package Main.src;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Disciplina disciplina = new Disciplina("D001", "Programação Orientada a Objetos", "2025.1");
+        Professor professor = new Professor("Prof. Ana", "Centro de Tecnologia");
+
+        Aluno aluno1 = new Aluno("João", "A001");
+        Aluno aluno2 = new Aluno("Maria", "A002");
+
+        Turma turma = new Turma("T01", disciplina, professor);
+        turma.adicionarAluno(aluno1);
+        turma.adicionarAluno(aluno2);
+
+        turma.imprimirInformacoes();
     }
 }
